e99900a093435cfaf17d7413f4b61bfcbe8f3008
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-09 22:17:49 +0300

    added a task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,3 +1,4 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :"name,", :description, :approximate_comp_time
+  attributes :id, :name, :description, :aproximate_comp_date
+  has_one :project
 end
