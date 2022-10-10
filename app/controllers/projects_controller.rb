44544eebb55eb3e3f4a234144a9e11b73c277d06
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-10 10:15:44 +0300

    added index and create functions to respective controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :handle_blanks
+
+    before_action :authorize
+    def index
+        projects = Project.all
+        render json: projects, status: :created
+    end
+
+    def create
+        project = Project.create!(project_params)
+        if project.valid?
+            render json: project, status: :created
+        else 
+            render json: {error: project.errors.full_messages}, status: :unprocessable_entity  
+        end
+    end
+
+    private
+    def project_params
+        params.permit(:title, :description, :user)
+    end
+
+    def handle_blanks(invalid)
+        render json: {error: invalid.record.errors}, status: :unprocessable_entity
+    end
+    def authorize
+        render json: {error: "Not authorized"}, status: :unauthorized unless session.include? user_id
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :handle_blanks
+
+    def show
+        user = User.find_by(id: session[:user_id]) 
+        if user
+            render json: user, status: :created
+        else 
+            render json: {error: "Not authorised"}, status: :unauthorized
+        end
+    end
+
+    def created
+        user = User.create!(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else            
+            render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :bio)
+    end
+
+    def handle_blanks(invalid)
+        render json:{error: invalid.record.errors}, status: :unprocessable_entity
+    end
 end
