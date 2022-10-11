e2ffbb8ed0bbe2d94fd2daadf239da4a2f41c80a
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-11 07:10:25 +0300

    Included sidebar data

---
+++ client/src/components/sidebar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import * as IoIcons from 'react-icons/io'
+
+
+export const SideBarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Dashboard',
+        path: '/projects',
+        icon: <AiIcons.AiFillDashboard/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Settings',
+        path: '/',
+        icon: <AiIcons.AiFillSetting/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Log Out',
+        path: '/',
+        icon: <AiIcons.AiOutlineLogout/>,
+        cName: 'nav-text'
+    }
+]
