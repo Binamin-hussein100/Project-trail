401511b226789e6f3b22d1c0393db981bbc1b593
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-11 07:08:04 +0300

    set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/homePage';
 import Navigation from './components/navbar';
-
+import Project from './components/project';
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <>
-      <Navigation/>
-      <HomePage/>
-      
+    <>              
+        {/* <Navigation/>
+        <Routes>
+            <Route path='/' exact element={<HomePage/>}/>
+            <Route path='/projects' element={<Project/>}/>
+        </Routes> */}
+{/* 
+        <Router>
+          <Navigation/>
+              <Route path='/' exact component={HomePage}/>
+              <Route path='/projects' component={Project}/>
+        </Router> */}
+        
+        <BrowserRouter>
+        <Navigation/>
+            <Routes>
+                <Route path='/' exact element={<HomePage/>}/>
+                <Route path='/projects' element={<Project/>}/>
+            </Routes>
+        </BrowserRouter>
+            
     </>
   );
 }
