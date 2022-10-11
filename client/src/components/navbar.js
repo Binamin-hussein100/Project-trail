8fa93a0c5f30168267fbe7235d0e63f2e6d248b2
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-11 07:09:19 +0300

    Set up the navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import  NavDropdown  from 'react-bootstrap/NavDropdown';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import  {SideBarData}   from './sidebar'
+
 
 const Navigation = () => {
-    return (
-          <Navbar>
+    const [sidebar, setSidebar] = useState(false)
 
-          </Navbar>
+    const showSidebar = () => setSidebar(!sidebar)
+
+    return (
+         <>
+            <div className='navbar'>
+                <Link to='#' className='menu_bars'>
+                    <FaIcons.FaBars onClick={showSidebar}/>
+                </Link>
+            </div>
+            <nav className={sidebar ? 'nav-menu active':'nav-menu'}>
+                <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className='navbar-toggle'>
+                        <Link to='#' className='menu-bars'>
+                            <AiIcons.AiOutlineClose/>
+                        </Link>
+                    </li>
+                    {SideBarData.map((item, index)=>{
+                        return(
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+         </>
     )
 }
 
