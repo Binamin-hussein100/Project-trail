30139c8f76da16a328f4f39e5a762583723263ef
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-13 22:06:09 +0300

    added a footer

--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import pic from '../images/organize.jpg'
+import pic1 from '../images/office.jpg'
 import * as GrIcons from 'react-icons/gr'
 import Button from 'react-bootstrap/Button';
 
@@ -25,7 +26,7 @@ const HomePage = () => {
                     <div className='text-center' id='launchD'>
                         <h3 id='launch'>WELCOME TO PROJECT ALLIANCE <GrIcons.GrLaunch/></h3>
                         <p>Project planning taken to the next level.</p>
-                        <div>
+                        {/* <div>
                             <h1 >Get started today!</h1>
                            
                             <form>
@@ -33,7 +34,8 @@ const HomePage = () => {
                                 <input className='' placeholder='email@domail.com' />
                                 <Button variant='outline-dark'>Get started!</Button>
                             </form>
-                        </div>
+                        </div> */}
+                        
                     </div>
                     </Col> 
                 </Row>                
@@ -46,8 +48,41 @@ const HomePage = () => {
                 </Row>
             </Container>
             <Container>
-                <Row>
-                    
+                <Row id='row2'>
+                    <Col sm={6}>
+                        <h4 id='intro3'>Better organization to get focused</h4>
+                        <p id='intro2'>
+                            <span>Keep your team on the rails</span><p>
+                                Tracker's shared backlogs makes priorities clear so the team can get organized. Easily vitualize scope, focus your team and stay nimble when circumstances change. 
+                            </p>
+                        </p>
+                    </Col>
+                    <Col sm={6}>
+                        <img src={pic1} alt='Project manager'/>
+                    </Col>
+                </Row>
+            </Container>
+            <Container fluid>
+                <Row id='row4'>
+                    <Col className='foot' sm={4}>
+                        <h5>Community</h5>
+                        <p>Blog</p>
+                        <p>Events</p>
+                        <p>BuildTV</p>
+                        <p>Consultancy Directory</p>
+                    </Col>
+                    <Col className='foot' sm={4}>
+                        <h5>About Us</h5>
+                        <p>Team</p>
+                        <p>Jobs</p>
+                        <p>Branding Guidelines</p>
+                    </Col>
+                    <Col className='foot' sm={4}>
+                        <h5>Support</h5>
+                        <p>Help Center</p>
+                        <p>Contact Center</p>
+                        <p>System status</p>
+                    </Col>
                 </Row>
             </Container>
 
