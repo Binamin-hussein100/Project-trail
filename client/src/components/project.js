bc8cf640cad6fa4d0404820bc8a5e9c5616a119c
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-13 22:07:10 +0300

    added tasks projects and Goals

--- client/src/components/project.js
+++ client/src/components/project.js
@@ -1,8 +1,54 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import * as BsIcons from "react-icons/bs";
+
 
 const Project = ()=>{
     return (
-        <h1>Project</h1>
+        <>
+            <Container  className="cont1">
+                <Row>
+                    <Col sm={4}></Col>
+                    <Col sm={6}>
+                        <h2>Hello, Binamin (O tasks)</h2>
+
+                    </Col>
+                    <Col sm={2}></Col>
+                </Row>
+            </Container>
+            <Container id="tasks" className="cont1">
+                <h3 id="priority">Priorities  <span><BsIcons.BsFillPlusCircleFill/></span></h3>
+                <Row>
+                    <Col sm={4}><u>Upcoming</u></Col>
+                    <Col sm={4}><u>Overdue</u></Col>
+                    <Col sm={4}><u>Completed</u></Col>
+                </Row>
+            </Container>
+            <Container id="project" className="cont2">
+            <h3 id="prj">Projects <span><BsIcons.BsFillPlusCircleFill/></span></h3>
+                <Row>
+                    <Col sm={12}>
+                        <ul>
+                            <li>one </li>
+                            <li>Two</li>
+                            <li>Three</li>                         
+                        </ul>
+                        
+                    </Col>
+                </Row>
+            </Container>
+            <Container id="goals" className="cont1">
+            <h3 id="prj">Goals<span><BsIcons.BsFillPlusCircleFill/></span></h3>
+                <Row>
+                    <Col>
+                        
+                    </Col>
+                </Row>
+            </Container>
+        </>     
+        
     )
 }
 
