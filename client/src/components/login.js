9da9ee9dfeebea341397e56f3ed9374da06afb26
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-15 11:02:32 +0300

    set up the login form

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import pic2 from '../images/unlock.jpg'
+
 
 const Login =()=>{
     return (
-        <Container >
-            
-            <Row id='signin'>
-                <Col sm={3}></Col>
-                <Col sm={6}>
-                <h3>Log in to your Account.</h3>
-                <Form>
-                    <Form.Group>
-                        <Form.Label>Username:</Form.Label>
-                        <Form.Control type='text'/>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Password:</Form.Label>
-                        <Form.Control type='password'/>
-                    </Form.Group>
-                    <Button variant='outline-dark'>Sign in</Button>
-                </Form>
-                </Col>
-                <Col sm={3}></Col>
-            </Row>
-        </Container>
+            <>
+                <div className='loginView'>
+                    <div className='signin'>
+                        <div className='lg'>
+                            <img src={pic2}/>
+                        </div>
+                        <div id='lgForm'>
+                            <h3 id='logTitle' className='text-center'>Log in to your Account.</h3>
+                            <div className='formy'>
+                                <Form>
+                                    <Form.Group>
+                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Control type='text'/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Password:</Form.Label>
+                                        <Form.Control type='password'/>
+                                    </Form.Group>
+                                    <Button id='signinSub' className='text-center' variant='outline-dark'>Sign in</Button>
+                                </Form>
+                            </div>
+                            <div id='dha' className='text-center'>
+                                <h5>Don't have an Account? Sign Up</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
     )
 }
 
