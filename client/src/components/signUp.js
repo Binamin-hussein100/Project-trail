a8524c0d72a8793bab47a8927f2e99fc186ead79
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-15 11:03:09 +0300

    added sign up form

--- client/src/components/signUp.js
+++ client/src/components/signUp.js
@@ -1,33 +1,45 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import pic3 from '../images/reg.jpg'
+
 
 const SignUp = () =>{
     return (
-        <Container>
-            <Form>
-                <Form.Group>
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control type='text'/>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Phone number:</Form.Label>
-                    <Form.Control type='number'/>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type='password'/>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type='password'/>
-                </Form.Group>
-                <Button id='btn' variant="outline-dark" type='submit'>Sign Up</Button>
-            </Form>
-        </Container>
+        <div className='loginView'>
+            <div className='signin'>
+                <div>
+                    <div className='lg'>
+                        <img src={pic3}/>
+                    </div>
+                </div>
+                <div>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control type='text'/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Phone number:</Form.Label>
+                            <Form.Control type='number'/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password'/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type='password'/>
+                        </Form.Group>
+                        <Button id='btn' variant="outline-dark" type='submit'>Sign Up</Button>
+                </Form>
+                </div>
+            </div>
+        </div>
         
     )
 }
 
 export default SignUp;
+
+
