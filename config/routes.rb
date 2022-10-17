3d1475532edaa31af80c3bc9cac2d39c76b77f72
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-17 09:33:21 +0300

    added paths/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   
-  resources :tasks
-  resources :projects
-  resources :users
+  resources :tasks, only: [:index, :create , :update, :show, :destory]
+  resources :projects ,only: [:index, :create , :update, :show, :destory]
+  resources :users ,only: [:index, :create, :update, :show, :destory]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
+  post "/projects", to: "projects#index"
+  post "/projects", to: "projects#create"
+
+
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
