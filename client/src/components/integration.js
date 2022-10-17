42124d30db8883de344101ce45e79262d73b93c0
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-17 09:31:48 +0300

    Improved responsiveness

--- client/src/components/integration.js
+++ client/src/components/integration.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col  from 'react-bootstrap/Col';
+import picA from '../images/construction.jpg'
 
 const Integration = () => {
     return (
-        <h1>Integration page</h1>
+        <>
+            <Container id='construction'>
+                <Row className='const1'>
+                    <Col sm={12}>
+                        <h2>Oooops! Sorry. This page is under construction.</h2>
+                        <img src={picA} alt='Construction' />            
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
 
