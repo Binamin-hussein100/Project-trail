86a4f3e8424b40156b9bc567afcf45cef8337c94
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-17 17:00:02 +0300

    added has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_password_secure
-    validates :username, presence: true uniqueness: true
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
     has_many :projects
 end
