58245ce34800370ed5c185d1606f6838c12bb0c5
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-18 13:42:00 +0300

    added index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :handle_blanks
 
+    def index
+        users = User.all 
+        render json: users
+    end
+
     def show
         user = User.find_by(id: session[:user_id]) 
         if user
