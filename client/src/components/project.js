26e2c9741c06de1bfc73bf49e9bdcb13aabfa966
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-23 20:57:24 +0300

    fetched projects

--- client/src/components/project.js
+++ client/src/components/project.js
@@ -3,6 +3,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import * as BsIcons from "react-icons/bs";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 
 
 const Project = ()=>{
@@ -18,6 +21,15 @@ const Project = ()=>{
     console.log(projects.length)
     console.log(projects)
 
+    const handleDeleteProject = (id) => {
+        
+            fetch(`/projects/${id}`,{
+                method: 'DELETE',
+            })
+    }
+
+
+
     return (
         <div>
             <ul>                            
@@ -31,21 +43,53 @@ const Project = ()=>{
                             <Col sm={2}></Col>
                         </Row>
                     </Container>
+                    
                     <Container id="tasks" className="cont1">
                             <h3 id="priority">Priorities  <span><BsIcons.BsFillPlusCircleFill /></span></h3>
                             <Row>
-                                <Col sm={4}><u>Upcoming</u></Col>
-                                <Col sm={4}><u>Overdue</u></Col>
-                                <Col sm={4}><u>Completed</u></Col>
+                                <Col sm={12}><u>All projects</u></Col>
+                                
+                                
                             </Row>
-                        </Container><Container id="project" className="cont2">
+                        </Container>
+                        <Container id="project" className="cont2">
                             <h3 id="prj">Projects <span><BsIcons.BsFillPlusCircleFill /></span></h3>
                             <Row>
-                                <Col sm={12}>
-                                    <ul>
-                                        <li>one </li>
-                                        <li>Two</li>
-                                        <li>Three</li>
+                                <Col sm={7}>
+                              
+                                    {/* <Card>
+                                        <Card.Header>Binamin</Card.Header>
+                                        <Card.Body>
+                                            <Card.Title>title</Card.Title>
+                                            <Card.Text>description</Card.Text>
+                                            <Button>DELETE</Button>
+                                            <Button>UPDATE</Button>
+                                        </Card.Body>
+                                    </Card> */}
+                        
+                                    <ul>                                        
+                                        {projects?.map(prj=>(
+                                            <li key={prj.id}>
+                                               
+                                                <Card border="dark">
+                                                <Card.Header>{prj.user.username}</Card.Header>
+                                                <Card.Body>
+                                                    <Card.Title>{prj.title}</Card.Title>
+                                                    <Card.Text>{prj.description}</Card.Text>
+                                                    <div className='optio'>
+                                                    <div>                              
+                                                        <Button variant='outline-dark'>UPDATE</Button>
+                                                    </div>
+                                                    <div>
+                                                        <Button variant='outline-danger' onClick={() => handleDeleteProject(prj.id)}>DELETE</Button>
+                                                    </div>
+                                                </div>
+                                                </Card.Body>
+                                            </Card>
+                                            </li>
+                                        
+                                ))}                                   
+                                        
                                     </ul>
 
                                 </Col>
