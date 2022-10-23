891b03c96e7f5cc61e386d0100d0e35738cedf36
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-23 20:52:30 +0300

    fetched all projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :handle_blanks
 
-    before_action :authorize
+    # before_action :authorize
     def index
         projects = Project.all
         render json: projects, status: :created
@@ -13,6 +13,16 @@ rescue_from ActiveRecord::RecordInvalid, with: :handle_blanks
             render json: project, status: :created
         else 
             render json: {error: project.errors.full_messages}, status: :unprocessable_entity  
+        end
+    end
+
+    def destroy
+        project = Project.find_by(id: params[:id])
+        if project
+            project.destroy
+            head :no_content
+        else  
+            render json: {error: "Project not found"}, status: :not_found
         end
     end
 
@@ -28,3 +38,5 @@ rescue_from ActiveRecord::RecordInvalid, with: :handle_blanks
         render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
     end
 end
+
+
