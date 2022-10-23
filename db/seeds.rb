c4bcaa2177817c93ce70b044ff5ae325877420f8
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-10-23 06:49:25 +0300

    seed data for project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+project = Project.create(title: "Car_treck", description: "E-commercefor Cars", user_id:1)
