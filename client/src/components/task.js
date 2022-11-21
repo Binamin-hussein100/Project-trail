098abe6212fdfd5da3f1e2d91dc4242b798d0140
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-11-21 15:49:32 +0300

    added get request to /tasks

--- client/src/components/task.js
+++ client/src/components/task.js
@@ -2,10 +2,21 @@ import React, {useState, useEffect} from 'react';
 
 
 const Task = ()=>{
-
+    const [task, setTask] = useState([])
    
+    useEffect(()=>{
+        fetch("/tasks")
+        .then((res)=>res.json())
+        .then((dta)=>setTask(dta))
+    },[])
+    console.log(task)
     return (
-        <h1>Task</h1>
+       
+        <ul>
+            {task?.map((i)=>(
+                <h6>{i.name}</h6>
+            ))}
+        </ul>
     )
 }
 
