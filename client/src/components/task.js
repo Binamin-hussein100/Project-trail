932dbdd2237b4e6fc85b836571c41c1c7c8449d2
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-11-22 08:59:43 +0300

    added task edit form

--- client/src/components/task.js
+++ client/src/components/task.js
@@ -1,20 +1,48 @@
 import React, {useState, useEffect} from 'react';
+import * as ImIcons from 'react-icons/im'
+import Card from 'react-bootstrap/Card'
+import UpdateTask from './updateTask';
+import Button from 'react-bootstrap/Button';
 
 
-const Task = ()=>{
+const Task = (props)=>{
     const [task, setTask] = useState([])
+    const [tskUpdate, setTskUpdate] = useState(false)
    
     useEffect(()=>{
         fetch("/tasks")
         .then((res)=>res.json())
         .then((dta)=>setTask(dta))
     },[])
-    console.log(task)
+    
+
+    const handleDeleteTask = (id) => {
+        fetch(`/tasks/${id}`,{
+            method: 'DELETE',
+        })
+        window.location.reload()
+    }
     return (
        
         <ul>
             {task?.map((i)=>(
-                <h6>{i.name}</h6>
+                // <Container id='tasky'>
+                //     <Row>
+                //         <Col sm={5}>{i.name}   {i.description}</Col>
+                //         <Col sm={5}>{i.timeAlloc}</Col>
+                //     </Row>
+                // </Container>
+               <li key={i.id}>
+                    <div id='tasker'>
+                        <h3>{i.name}</h3>
+                        <h5>{i.description}</h5>                        
+                        <div id='btns'>                              
+                            <Button variant='outline-dark' onClick={()=> setTskUpdate(true)}>UPDATE</Button>
+                            <UpdateTask onHide = {()=>setTskUpdate(false)} show={tskUpdate} user = {props.user} /> 
+                            <ImIcons.ImCancelCircle variant='outline-danger' onClick={() => handleDeleteTask(i.id)}>DELETE</ImIcons.ImCancelCircle>
+                        </div>
+                    </div>
+               </li>
             ))}
         </ul>
     )
